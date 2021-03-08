a5ae87ac320481aeb3ffd2ef9fe21c479ed2b8eb
Odelon Jhey Pacalso <odjhey@gmail.com>
2021-03-08 15:04:23 +0800

    add promise wrapper for dart futures

--- index.dart
+++ index.dart
@@ -1,4 +1,5 @@
 import 'package:js/js.dart';
+import 'package:http/http.dart' as http;
 
 @JS()
 class Request {
@@ -16,19 +17,52 @@ external String stringify(Object obj);
 @JS()
 class FetchEvent {
   external Request get request;
-  external void respondWith(Response r);
+  external void respondWith(Promise<Response> r);
 }
 
 @JS()
 external void addEventListener(String type, void Function(FetchEvent event));
 
+@JS()
+class Promise<T> {
+  external Promise(void executor(void resolve(T result), Function reject));
+  external Promise then(void onFulfilled(T result), [Function onRejected]);
+}
+
 void main() {
-  addEventListener('fetch', allowInterop((FetchEvent event) {
-    event.respondWith(handleRequest(event.request));
+  addEventListener('fetch', allowInterop((FetchEvent event) async {
+    /*
+    handleRequest(event.request).then((res) {
+      print('returned');
+      print(res);
+      event.respondWith(res);
+    });
+    */
+    //event.respondWith(new Response("hi"));
+    /*
+    event.respondWith(new Future(() {
+      print('yaharu');
+      return new Response("yaharu");
+    }));
+    */
+
+    event.respondWith(new Promise(allowInterop((resolve, reject) {
+      new Future(() {
+        print('yaharu1');
+        return new Response("yaharu1");
+      }).then(resolve, onError: reject);
+    })));
+
+    /*
+    return new Future(() {
+      print('value ');
+      event.respondWith(new Response("Hello"));
+    });
+    */
   }));
 }
 
-Response handleRequest(Request request) {
+Future<Response> handleRequest(Request request) async {
   print(stringify(request));
   var uri = Uri.parse(request.url);
   uri.pathSegments.forEach((k) {
@@ -37,5 +71,12 @@ Response handleRequest(Request request) {
   uri.queryParameters.forEach((k, v) {
     print('key: $k - value: $v');
   });
+  print('request');
+
+/*
+  var res =
+      await http.get(Uri.https('jsonplaceholder.typicode.com', 'albums/1'));
+      */
+
   return new Response("Dart Workers world!");
 }
